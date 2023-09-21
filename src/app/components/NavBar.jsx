7394a58e67e1faeee700aa93b09d8beae71a4077
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-09-21 11:10:28 -0400

    initializing navbar component

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react'
+
+
+export const NavBar = () => {
+  return (
+    <nav>
+      <div>
+
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
