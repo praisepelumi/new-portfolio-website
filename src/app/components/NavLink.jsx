432eabfe38fddd12165094d7ddc79dc749cbddfa
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-09-23 19:37:54 -0400

    fixed hover transition for about section

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const NavLink = ({ href, title }) => {
   return (
-    <Link href={href} className="block py-2 pl-3 pr-4 text-[#F5F5DC] sm:text-l font-semibold rounded md:p-0 hover:bg-secondary-color hover:text-primary-color hover:p-1">
+    <Link href={href} className="block py-2 pl-3 pr-4 text-[#F5F5DC] sm:text-l font-semibold rounded md:p-1 hover:bg-secondary-color hover:text-primary-color transition duration-300">
       {title}
     </Link>
   );
