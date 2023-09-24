9e375e5a61370a6c1318b60b3b081d05b9a96738
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-09-23 23:08:51 -0400

    created projects component

---
+++ src/app/components/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Projects = ({ imgUrl, title, description}) => {
+  return (
+    <div>
+      <div className='h-52 md:h-72' style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}></div>
+      <div className='text-primary-color'>
+        <h5>{title}</h5>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Projects;
