ba78c4c8e7d1e059e0ce21b2fe1acb0dcbd40dec
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-09-23 23:19:37 -0400

    added projects data

--- src/app/components/ProjectsComponent.jsx
+++ src/app/components/ProjectsComponent.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
+import ProjectsInfo from './ProjectsInfo'
 
-const ProjectsComponent = ({ imgUrl, title, description}) => {
+const projectsData = [
+  {
+    id: 1,
+    title: "MonDinero",
+    description: "MonDinero is a budgeting app that takes advantage of D3 to display your budget and expenses side by side in interactive and easy to read graphs.",
+    image: "/images/projectImages/mondinero.png"
+  },
+  {
+    id: 2,
+    title: "Kubernét",
+    description: "Kubernét is a user-friendly solution for effortlessly visualizing and monitoring your Kubernetes metrics in real-time.",
+    image: "/images/projectImages/dashboard.gif"
+  },
+  {
+    id: 3,
+    title: "CallStack",
+    description: "Created by developers for developers. A community for everyone who ♥'s code. Learn together, grow together.",
+    image: "/images/projectImages/callstack.png"
+  },
+  {
+    id: 4,
+    title: "Github",
+    description: "View more projects on Github",
+    image: "/images/PRAISE.png"
+  },
+]
+
+const ProjectsComponent = () => {
   return (
-    <div>
-      <div className='h-52 md:h-72' style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}></div>
-      <div className='text-primary-color'>
-        <h5>{title}</h5>
-        <p>{description}</p>
-      </div>
-    </div>
+    <><div><h2>Open Source and Projects</h2></div></>
   )
 }
 
-export default ProjectsComponent;
+export default ProjectsComponent
