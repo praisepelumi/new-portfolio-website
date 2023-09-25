3683c1a3d441b98b8a4cfb73a14258f523cf5494
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-09-24 21:10:46 -0400

    finished current version of website

--- src/app/components/ProjectsInfo.jsx
+++ src/app/components/ProjectsInfo.jsx
@@ -7,7 +7,7 @@ const ProjectsInfo = ({ imgUrl, title, description, gitUrl }) => {
     <div>
       <div 
         className='h-52 md:h-72 rounded-t-xl relative group'
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center"}}
       >
         <div className='overlay flex items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-40 transition-all duration-600 cursor-pointer'>
           <Link href={gitUrl} target="_blank" className="h-14 w-14 relative">
