f0797cc75eeea4bed395a05911962f47ac8869e7
Praise Emmanuel <praisepemmanuel@gmail.com>
2023-09-24 20:14:47 -0400

    added contact link

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -9,18 +9,22 @@ const navLinks = [
   {
   title: "About",
   path: "#about",
+  target: "_self"
   },
   {
   title: "Resume",
-  path: "#about",
+  path: "/Praise_Emmanuel_Resume.pdf",
+  target: "_blank"
   },
   {
   title: "Projects",
   path: "#projects",
+  target: "_self"
   },
   {
-  contact: "Contact",
-  path: "/",
+  title: "Contact",
+  path: "https://mail.google.com/mail/?view=cm&fs=1&to=praisepemmanuel@gmail.com",
+  target: "_blank"
   }
 ];
 
@@ -30,7 +34,7 @@ export const NavBar = () => {
   return (
     <nav className='fixed top-0 left-0 right-0 z-10 bg-primary-color bg-opacity-100'>
       <div className='flex flex-wrap items-center justify-between mx-auto px-6 py-2'>
-        <Link href={"/"} className='text-xl md:text-2xl text-[#F5F5DC] font-bold'>
+        <Link href={"/"} onClick={() => window.location.reload()} target="_self" className='text-xl md:text-2xl text-[#F5F5DC] font-bold'>
         &lt;Praise/&gt;
         </Link>
         <div className='mobile-menu block md:hidden'>
@@ -51,7 +55,7 @@ export const NavBar = () => {
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
            {navLinks.map((link, index) => (
             <li key={index}>
-              <NavLink href={link.path} title={link.title}/>
+              <NavLink href={link.path} title={link.title} target={link.target}/>
             </li>
           ))}
           </ul>
